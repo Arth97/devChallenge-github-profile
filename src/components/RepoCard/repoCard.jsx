46971e1aeba995d8edfc4829db7ced773e5c8c4a
Arturo <arturotinajas@gmail.com>
2025-04-29 21:48:10 +0200

    .

--- src/components/RepoCard/repoCard.jsx
+++ src/components/RepoCard/repoCard.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import './repoCard.css';
 
-const RepoCard = () => {
+const RepoCard = ({repoData}) => {
   return (
     <div className="repo-card">
       <p className="title-text">CardTitle</p>
       <p className="body-text">Description</p>
 			<div className="flex flex-row gap-2">
-				<div className="body-text">mit</div>
-				<div className="body-text">action-fork</div>
-				<div className="body-text">star</div>
-				<div className="samll-text">status</div>
+				<div className="body-text">
+					<img src="/Chield_alt.svg" alt="Chield" className="chield-icon" />
+					<span>Mit</span>
+				</div>
+				<div className="body-text">
+					<img src="/Nesting.svg" alt="Nesting" className="nesting-icon" />
+					<span>Forks</span>
+				</div>
+				<div className="body-text">
+					<img src="/Star.svg" alt="Star" className="star-icon" />
+					<span>Stars</span>
+				</div>
+				<div className="small-text">
+					status
+					<span></span>
+				</div>
 			</div>
     </div>
   );
